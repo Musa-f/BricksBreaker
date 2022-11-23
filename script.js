a1c7dcb1386669d3f37580418dd3193e73e2311e
musa-f <musaevafatima77@gmail.com>
2022-11-23 01:02:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//------------------Couleur random des cubes
+let cubes = [...document.querySelectorAll('div.cube')];
+let colorArray = ['#19A600', '#9892F0', '#8BF07A', '#F09762', '#A36C4B']
+
+let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)]
+
+for(let i=0;i<cubes.length;i++){
+    cubes[i].style.background = randomColor;
+}
+
+//---------------------Curseur de la barre
+let cursor = document.getElementById('barre');
+let container = document.getElementById('container');
+container.addEventListener('mousemove', function(e){
+    cursor.style.left = e.clientX + "px";
+});
+
+
+//---------------------Ball qui suit
+let ball = document.getElementById('ball');
